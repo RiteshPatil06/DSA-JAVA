cd72143ad76c5e37b7dd60fd0a6bdf18fd226cde
Ritesh Patil <patilritesh4016@gmail.com>
2024-09-20 19:37:06 +0530

    patterns

--- day1/basics.java
+++ day1/basics.java
@@ -1,3 +1,40 @@
-public class basics {
-    
+//class - an entity with many functions
+class Basics { 
+    //function
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
+/////////////////////
+
+//Variables
+        // String name = "Ritesh";
+        // double price = 25.25;
+        // boolean isAvailable = true;
+        // int a = 2;
+        // int b = 25;
+        // int summ = a + b;
+
+//Input
+//Import java.util.*;
+// Scanner sc = new Scanner(System.in);
+// String name = sc.nextLine();
+// System.out.println(name);
+
+//next()
+//nextLine()
+//nextInt()
+//nextFloat()
+//nextDouble()
+//nextBoolean()
+
+//conditional statement
+//if else
+//if else if
+//switch break
+
+//loops
+//for loop
+//while loop
+//do while loop
+ 
