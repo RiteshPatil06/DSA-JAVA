122cd44a96363e980b23d9ba0a4f018d80135b4e
Ritesh Patil <patilritesh4016@gmail.com>
2024-09-22 14:58:36 +0530

    done

---
+++ day3/Pattern12.java
@@ -0,0 +1,28 @@
+//Diamond pattern
+public class Pattern12 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+        //spaces
+        for (int j = 1; j <= n - i; j++) {
+            System.out.print(" ");
+        }
+        //stars
+        for (int k = 1; k <= 2*i-1; k++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        }
+        for (int i = n; i >= 1; i--) {
+            //spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            //stars
+            for (int k = 1; k <= 2*i-1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            }
+    }
+}
